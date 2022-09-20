9ed734d4a38c891158daef15153db39d421fe3cb
Obiora Igboanusi <cwertlinks@gmail.com>
2022-09-20 13:13:18 +0100

    Feat: Implement  routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
 import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
   );
 }
 
