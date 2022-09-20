07b7a4ef92a97bd66cdad4be3b3e18fc6afcf548
Obiora Igboanusi <cwertlinks@gmail.com>
2022-09-20 14:44:33 +0100

    Feat: Create redux store

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
 import SignIn from "./pages/SignIn";
 
+import { Provider } from "react-redux";
+import { store } from "./application/store";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Provider>
   );
 }
 
