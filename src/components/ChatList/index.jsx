47672e9d0de2bfccc4e654937a2075f8b7d58af9
Obiora Igboanusi <cwertlinks@gmail.com>
2022-09-21 11:03:12 +0100

    Feat: Add load more  chats

--- src/components/ChatList/index.jsx
+++ src/components/ChatList/index.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import useScrollChat from "../../utils/Hooks/useScrollChat";
 import ChatItem from "../ChatItem";
 
+const chatPerPage = 27;
+
 function ChatList() {
   const { chats, auth } = useSelector((state) => state);
+  const [sizeToLoad, setSizeToLoad] = useState(chatPerPage);
   const ref = useScrollChat(chats.length);
+
+  const handleLoadMore = useCallback(() => {
+    setSizeToLoad((prev) => prev + chatPerPage);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sizeToLoad]);
 
   return (
     <div>
       <StyledList ref={ref}>
-        {chats.map((chat) => (
+        {chats.length > sizeToLoad && (
+          <li key="load-more" className="load-more">
+            <button onClick={handleLoadMore}>Load more</button>
+          </li>
+        )}
+        {chats.slice(-sizeToLoad).map((chat) => (
           <li key={chat.id}>
             <ChatItem
               {...{ ...chat, isAuthor: chat.author === auth.username }}
@@ -28,6 +41,15 @@ const StyledList = styled.ul`
   overflow-y: auto;
   li {
     margin-bottom: 5px;
+  }
+  li.load-more {
+    text-align: center;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    margin: 5px;
+    font-weight: bold;
   }
 `;
 export default ChatList;
